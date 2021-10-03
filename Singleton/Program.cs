153f2e58bbbdb172401e566d5bc801ad0e5ce548
muratcant <muratcantanriverdi96@gmail.com>
2021-10-03 17:28:52 +0300

    Add project files.

---
+++ Singleton/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = SingletonObject.GetInstance();
+            data.PrintName();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Singleton/SingletonObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class SingletonObject
+    {
+        private static SingletonObject obj = new SingletonObject();
+        private static int count;
+        private readonly String name;
+
+        private SingletonObject()
+        {
+            count++;
+            name = "Singleton"+count;
+        }
+
+        public static SingletonObject GetInstance()
+        {
+            return obj;
+        }
+
+        public void PrintName()
+        {
+            Console.WriteLine(name);
+        }
+
+    }
+}
